80dbbc31e00e710d62a5531668f6f6d2d4b088a0
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2019-03-10 22:28:30 -0500

    change scroll coloring

--- Candy_Museum/index.js
+++ Candy_Museum/index.js
@@ -1,13 +1,10 @@
 const navbar = document.getElementById('navbar');
-const main = document.getElementById('main');
 
-// the alternative to using the global context would be to create a context with a function that returns a function (as in "debounce") -- probably a good exercise, but given the scope of this script it seems superfluous.
-let timeout;
-
-window.addEventListener('scroll', function() {
-  navbar.classList.add('scrolled');
-  if (timeout) { clearTimeout(timeout); }
-  timeout = setTimeout(function() {
+window.addEventListener('scroll', function(e) {
+  const scrollTop = e.target.scrollingElement.scrollTop;
+  if (scrollTop > 0) {
+    navbar.classList.add('scrolled');
+  } else {
     navbar.classList.remove('scrolled');
-  },400)
+  }
 });
